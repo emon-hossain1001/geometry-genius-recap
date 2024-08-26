c3154b0557203d649f98747c2510f914f9f08a64
emon-hossain1001 <mrx00001001@gmail.com>
2024-08-27 04:17:12 +0600

    added rhombus

---
+++ js/rhombus.js
@@ -0,0 +1,18 @@
+document.getElementById('btn-rhombus').addEventListener('click', () => {
+    const d1 = getInputValueById('rhombus-d1');
+    const d2 = getInputValueById('rhombus-d2');
+
+    const area = document.createElement('p');
+    area.innerHTML = `Rhombus Area = ${0.5 * d1 * d2} cm<sup>2</sup>`;
+
+    const areaContainer = document.getElementById('area-container');
+    areaContainer.appendChild(area);
+});
+
+function getInputValueById(inputId){
+    const inputText = document.getElementById(inputId);
+    const inputValue = inputText.value;
+    const value = parseFloat(inputValue);
+    inputText.value = '';
+    return value;
+}
